7deed99b8077fc53bd63e556cf358efae910cabb
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-07-09 01:20:44 +0700

    finished

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const rateText = document.getElementById('rate');
 const swapBtn = document.getElementById('btn');
 
 from_currency.addEventListener('change', calculateMoney);
+to_currency.addEventListener('change', calculateMoney);
+from_currency_amount.addEventListener('input', calculateMoney);
+to_currency_amount.addEventListener('input', calculateMoney);
 
 
 function calculateMoney(){
@@ -16,9 +19,18 @@ function calculateMoney(){
     fetch(`https://v6.exchangerate-api.com/v6/d2ab3224f6350ab80d30b7e7/latest/${from}`)
     .then(res=>res.json()).then(data=>{
         const rate = data.conversion_rates[to];
-        rateText.innerText=`1 = ${from} = ${rate} ${to}`;
+        rateText.innerText=`1 ${from} = ${rate} ${to}`;
+        to_currency_amount.value=(from_currency_amount.value*rate);
     })
 }
+
+swapBtn.addEventListener('click',()=>{
+    //Created a variable to store the previous currency because if we don't do it, once we switch to another currency and want to go back to the previous one, it won't be possible. This is because the code will remember the original variable, which is the same.
+    const temp = from_currency.value;
+    from_currency.value = to_currency.value;
+    to_currency.value = temp;
+    calculateMoney();
+})
 
 calculateMoney();
 //so, when open the browser, it will automatically store the values of the from/to currencies.
